704394229506fad6e9e7d2eb63ca3e4cfb81ee2d
nikhilabhiman <nikhil.abhiman@techolution.com>
2018-04-19 22:34:08 -0400

    added tested code

--- src/main/java/com/techolution/bi/BiFiegnConfig.java
+++ src/main/java/com/techolution/bi/BiFiegnConfig.java
@@ -2,10 +2,12 @@ package com.techolution.bi;
 
 //import feign.Contract;
 import feign.Contract;
+import feign.RequestInterceptor;
+import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class BiFiegnConfig {
 
     HmacAuthorizationInterceptor interceptor;
@@ -23,4 +25,23 @@ public class BiFiegnConfig {
     public HmacAuthorizationInterceptor hmacAuthRequestInterceptor() {
         return interceptor;
     }
+
+    //    @Bean
+//    public RequestInterceptor requestInterceptor() {
+//
+//        return new BasicAuthRequestInterceptor("abc","def") {
+//
+//            @Override
+//            public void apply(RequestTemplate template) {
+//                template.header(HttpHeaders.AUTHORIZATION, "abcd");
+//            }
+//        };
+//    }
+
+//    @Bean
+//    public RequestInterceptor hmacAuthRequestInterceptor() {
+//        //return new HmacAuthorizationInterceptor("abc", "def");
+//        return new BasicAuthRequestInterceptor("abc", "def");
+//
+//    }
 }

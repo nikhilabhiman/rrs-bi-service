ba54448d280c55471c1a3f622d7d789eef217795
nikhilabhiman <nikhil.abhiman@techolution.com>
2018-04-19 22:18:35 -0400

    Initial commit

--- src/main/java/com/techolution/bi/BiAdapterApplication.java
+++ src/main/java/com/techolution/bi/BiAdapterApplication.java
@@ -1,6 +1,7 @@
 package com.techolution.bi;
 
-import lombok.val;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
